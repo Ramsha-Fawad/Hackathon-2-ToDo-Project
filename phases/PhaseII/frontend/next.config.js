76077a4c42c77f7662209b8267a699698d78e4a1
Ramsha-Fawad <ramsha.tahir23@gmail.com>
2026-02-07 20:41:17 +0500

    Delete phases/PhaseII/frontend directory

--- phases/PhaseII/frontend/next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Additional configuration
-  // Removed output: 'export' to allow dynamic routes
-};
-
-module.exports = nextConfig;
